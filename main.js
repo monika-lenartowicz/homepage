ec8d8cfd8cc23b24d7bfe1ad97380645982ef853
Monika Lenartowicz <nika.nel@interia.pl>
2020-01-07 15:52:34 +0100

    feat: zadanie domowe week4

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ console.log(age);
 
 console.log(`Nazywam się ${name} i mam ${age} lat`);
 
+console.log("witaj geniuszu, skoro tu zaglądasz!");
+
+const diary__coten=document.querySelector('.diary__coten');
+console.log(diary__coten);
+
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
+console.log(paragraphs[1]);
+
+console.log(diary__coten.innerHTML);
+diary__coten.innerHTML = "<strong>pozdro js<strong>";
+
