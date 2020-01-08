d84c692fbbf1031f8db957a4a13859bb459492c6
Monika Lenartowicz <nika.nel@interia.pl>
2020-01-08 19:53:32 +0100

    float: zadanie domowe w4

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ console.log(`Nazywam się ${name} i mam ${age} lat`);
 
 console.log("witaj geniuszu, skoro tu zaglądasz!");
 
-const diary__coten=document.querySelector('.diary__coten');
-console.log(diary__coten);
+const day__coten=document.querySelector('.day__coten');
+console.log(day__coten);
 
 const paragraphs = document.querySelectorAll('p');
 console.log(paragraphs);
 console.log(paragraphs[1]);
 
-console.log(diary__coten.innerHTML);
-diary__coten.innerHTML = "<strong>pozdro js<strong>";
+console.log(day__coten.innerHTML);
+day__coten.innerHTML = "<strong>pozdro js<strong>";
 
